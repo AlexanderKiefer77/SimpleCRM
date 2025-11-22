fb22e004baaa9efc1af9732d7782f6b9bdbd7630
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-22 11:24:03 +0100

    add sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule ],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
