5621050fdb672976582cd93c68dd9061949f1dbc
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-22 12:11:26 +0100

    routing to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { User } from './user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Dashboard },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'user', component: User },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -7,7 +7,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
