6a8401e05ced326182c5540f9c274a0bc63cf065
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-23 15:12:12 +0100

    vor Angular EInbindeversuch

--- src/app/user-detail/user-detail.ts
+++ src/app/user-detail/user-detail.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatMenuModule],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
 export class UserDetail {
 
+
+
 openAddressDialog() {};
+
+editUserDetail() {};
+
+editMenu() {};
+
 
 }
